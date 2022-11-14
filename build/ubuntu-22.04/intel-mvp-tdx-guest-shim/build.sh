b71b432f94140809ed1747f5b8dbc25395bd3e2d
gwang <gang2.wang@intel.com>
2022-11-14 18:33:32 +0800

    SDL: moddify issues by shellcheck snyk    ShellCheck advice summary:        1 cd xxx  suggests : cd xxx || exit        2 ${VAR}  suggests : "${VAR}"        3 ${USER}@$HOSTNAME suggests : "${USER}"@"$HOSTNAME"

--- build/ubuntu-22.04/intel-mvp-tdx-guest-shim/build.sh
+++ build/ubuntu-22.04/intel-mvp-tdx-guest-shim/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 CURR_DIR=$(dirname "$(readlink -f "$0")")
-UPSTREAM_URI="https://github.com/rhboot/shim/releases/download/15.4/shim-15.4.tar.bz2"
 UPSTREAM_VERSION="15.4"
 DOWNSTREAM_GIT_URI="https://github.com/intel/shim-tdx"
 DOWNSTREAM_TAG="tdx-guest-ubuntu-20.04-2021.09.27"
@@ -14,11 +13,11 @@ fi
 
 get_source() {
     echo "Get downstream source code..."
-    cd ${CURR_DIR}
+    cd "${CURR_DIR}" || exit
     if [[ ! -d ${PACKAGE}-${UPSTREAM_VERSION} ]]; then
         git clone --branch ${DOWNSTREAM_TAG} --single-branch --depth 1 \
             ${DOWNSTREAM_GIT_URI} ${PACKAGE}-${UPSTREAM_VERSION}
-        cd ${PACKAGE}-${UPSTREAM_VERSION}
+        cd ${PACKAGE}-${UPSTREAM_VERSION} || exit
         git submodule update --init
         rm ${PACKAGE}-${UPSTREAM_VERSION}/.git -fr
     fi
@@ -26,13 +25,13 @@ get_source() {
 
 prepare() {
     echo "Prepare..."
-    cp ${CURR_DIR}/debian/ ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION} -fr
-    cp ${CURR_DIR}/Makefile ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION}
+    cp "${CURR_DIR}"/debian/ "${CURR_DIR}"/${PACKAGE}-${UPSTREAM_VERSION} -fr
+    cp "${CURR_DIR}"/Makefile "${CURR_DIR}"/${PACKAGE}-${UPSTREAM_VERSION}
 }
 
 build() {
     echo "Build..."
-    cd ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION}
+    cd "${CURR_DIR}"/${PACKAGE}-${UPSTREAM_VERSION} || exit
     sudo mk-build-deps --install --build-dep '--tool=apt-get --no-install-recommends -y' debian/control
     # TODO: fail to build source, so build binary only first
     debuild -uc -us -i -I -b
