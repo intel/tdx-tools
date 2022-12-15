a28b4e53b4c27e373e5e97a0e7b52c120c52e1bd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-15 17:53:45 +0000

    Adding tarfile member sanitization to extractall()

--- utils/pycloudstack/pycloudstack/artifacts.py
+++ utils/pycloudstack/pycloudstack/artifacts.py
@@ -194,7 +194,26 @@ class Artifact:
             if is_download_new or not os.path.exists(cache_file[:-7]):
                 LOG.debug("Decompress file %s...", cache_file)
                 with tarfile.open(cache_file) as tarfd:
-                    tarfd.extractall(cache_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tarfd, cache_dir)
 
             cache_file = cache_file[:-7]
             dest_file = dest_file[:-7]
