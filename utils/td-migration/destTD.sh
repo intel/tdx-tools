3b5c8abb1556bc159203a6ceca61f14eaa80d8c3
LeiZhou <lei.zhou@intel.com>
2023-03-29 11:40:49 +0800

    TD-Migration: Fix the shell string split bug
    Signed-off-by: LeiZhou <lei.zhou@intel.com>

--- utils/td-migration/destTD.sh
+++ utils/td-migration/destTD.sh
@@ -83,26 +83,27 @@ error() {
 }
 
 launch_TDVM() {
-    QEMU_CMD="${QEMU_EXEC} 
-        -accel kvm 
-        -cpu host,pmu=off,-kvm-steal-time,-shstk,tsc-freq=1000000000 
-        -smp 4,threads=1,sockets=1 
-        -m 16G
-        -object tdx-guest,id=tdx0,sept-ve-disable=on,debug=off,migtd-pid=${TARGET_PID} 
-        -object memory-backend-memfd-private,id=ram1,size=16G
-        -machine q35,memory-backend=ram1,confidential-guest-support=tdx0,kernel_irqchip=split 
-        -bios ${OVMF} 
-        -chardev stdio,id=mux,mux=on,logfile=${LOG}
-        -drive file=$(readlink -f "${GUEST_IMG}"),if=virtio,id=virtio-disk0,format=qcow2 
-        -name process=lm-dst,debug-threads=on 
-        -no-hpet -nodefaults 
-        -D /run/qemu-dst.log -nographic -vga none 
-        -monitor unix:/tmp/qmp-sock-dst,server,nowait 
-        -device virtio-serial,romfile= 
-        -device virtconsole,chardev=mux -serial chardev:mux -monitor chardev:mux
-	    -device virtio-net-pci,netdev=mynet0,mac=00:16:3E:68:00:10,romfile=
-	    -netdev bridge,id=mynet0,br=virbr0
-		-incoming tcp:0:${INCOMING_PORT}"
+    QEMU_CMD="${QEMU_EXEC} \
+        -accel kvm \
+        -cpu host,pmu=off,-kvm-steal-time,-shstk,tsc-freq=1000000000 \
+        -smp 4,threads=1,sockets=1 \
+        -m 16G \
+        -object tdx-guest,id=tdx0,sept-ve-disable=on,debug=off,migtd-pid=${TARGET_PID} \
+        -object memory-backend-memfd-private,id=ram1,size=16G \
+        -machine q35,memory-backend=ram1,confidential-guest-support=tdx0,kernel_irqchip=split \
+        -bios ${OVMF} \
+        -chardev stdio,id=mux,mux=on,logfile=${LOG} \
+        -drive file=$(readlink -f "${GUEST_IMG}"),if=virtio,id=virtio-disk0,format=qcow2 \
+        -name process=lm-dst,debug-threads=on \
+        -no-hpet -nodefaults \
+        -D /run/qemu-dst.log -nographic -vga none \
+        -monitor unix:/tmp/qmp-sock-dst,server,nowait \
+        -device virtio-serial,romfile= \
+        -device virtconsole,chardev=mux -serial chardev:mux -monitor chardev:mux \
+        -device virtio-net-pci,netdev=mynet0,mac=00:16:3E:68:00:10,romfile= \
+        -netdev bridge,id=mynet0,br=virbr0 \
+        -incoming tcp:0:${INCOMING_PORT}"
+
     if [[ ${BOOT_TYPE} == "direct" ]]; then
         QEMU_CMD+=" -kernel $(readlink -f "${KERNEL}")"
         QEMU_CMD+=" -append \"root=${ROOT_PARTITION} rw console=hvc0\" "
